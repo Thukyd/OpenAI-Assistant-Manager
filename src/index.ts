8a795b2df40d0686d2f6df8c430aeb5404d77ed6
Jan-Philipp Tebbe <jptebbe@gmail.com>
2023-12-08 19:02:14 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,9 @@ import { AssistantCreateParams } from 'openai/resources/beta/assistants/assistan
 const assistantConfigs: AssistantCreateParams = {
     name: "Red Dead Redemption 2 - Poker Assistant",
     description: "",
-    instructions: "You are an AI assistant that analyzes screenshots from Red Dead Redemption 2 poker games...",
+    instructions: "You are an AI assistant that analyzes screenshots from Red Dead Redemption 2 poker games. Your task is to evaluate the current game situation from these images, offering strategic advice on poker hands and betting tactics. You educate players on evaluating cards and understanding game dynamics, guiding them towards making smart decisions. Your goal is to progressively teach players so they can eventually master poker strategy on their own.",
     model: "gpt-4-1106-preview",
-    tools: [{ "type": "code_interpreter" }],
+    tools: [{ "type": "retrieval" }],
     file_ids: [],
     metadata: {}
 };
@@ -16,7 +16,7 @@ const assistantConfigs: AssistantCreateParams = {
 // TODO: support file uploads for assistant creation
 
 async function main() {
-
+ 
     // either fetch the existing assistant or create a new one
     try {
         const assistant = await initializeAssistant(assistantConfigs);
